105e3c861a780812ef2936ad333d7020916621d2
paltrickontpb <paltrickontpb@users.noreply.github.com>
2017-03-26 03:49:42 +0530

    Updated read-ts-field.py

--- read-ts-field.py
+++ read-ts-field.py
@@ -1,9 +1,9 @@
 import thingspeak
 import simplejson as json
 
-CHANNEL_ID = '246127'
-WRITE_API_KEY  = 'FH09RW1R5V9QHGN5'
-READ_API_KEY   = '8E56MY9VB88SRGOK'
+CHANNEL_ID = ''
+WRITE_API_KEY  = ''
+READ_API_KEY   = ''
 
 
 channel = thingspeak.Channel(id=CHANNEL_ID,api_key=READ_API_KEY,write_key=WRITE_API_KEY)
